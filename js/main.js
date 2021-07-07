5db09fbcb43cdcfae6a8280f0e74f8538782c566
Beket <lbeketl@gmail.com>
2021-07-07 22:21:14 +0300

    Commit 2.08.27

--- js/main.js
+++ js/main.js
@@ -77,4 +77,23 @@ $(document).ready( () => {
             }
         }
     });
+
+    /*------------- Team Carousel --------------*/
+    $('.team-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
 });
